a13f60e5419acc064637fe8e76096a7d8a2061ad
Hanna-Kaarina <hanna.and@myy.haaga-helia.fi>
2022-01-27 21:19:27 +0200

    valmista tuli

--- App.js
+++ App.js
@@ -1,11 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [input1, setInput1] = useState('');
+  const [input2, setInput2] = useState('');
+  const [result, setResult] = useState('');
+  const [equations, setEquations] = useState([]);
+
+  const userInput1 = input1 => {
+    setInput1(input1);
+  }
+  const userInput2 = input2 => {   
+    setInput2(input2);
+  };
+
+    let symbol ='';
+    let equation = '';
+  
+  const buttonPressed1 = () => { 
+    let sum = parseFloat(input1) + parseFloat(input2);
+    createEquation(input1, input2, "+", sum);
+    setInput1('');
+    setInput2('');
+  };
+
+  const buttonPressed2 = () => { 
+    let minus = input1 - input2;
+    createEquation(input1, input2, "-", minus);
+    setInput1('');
+    setInput2('');
+  };
+
+  const createEquation = (input1, input2, symbol, res) => {
+    equation= input1 + " " + symbol + " " + input2 + " = " + res;
+    setResult(res);
+    setEquations([...equations,equation]);
+    console.log(equations);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titleText} >Result: { result } </Text>
+        <TextInput 
+              style={styles.input} 
+              keyboardType= 'numeric'
+              onChangeText={userInput1}
+              value={input1}/>
+      <TextInput 
+              style={styles.input} 
+              keyboardType= 'numeric'
+              onChangeText={userInput2}
+              value={input2}/>
+      <View style={{flexDirection: 'row', alignItems: 'center'}}>
+      <Button onPress={buttonPressed1} title="+" />
+      <Button onPress={buttonPressed2} title="-" />
+      </View>
+      <View style={styles.history}>
+        <Text style={{ fontSize: 22 }}>History</Text>
+        <FlatList
+          data={equations}
+          renderItem={({item}) => <Text>{item}</Text>}
+          keyExtractor={(item, index) => index.toString()}/>
+      </View>
     </View>
   );
 }
@@ -13,8 +71,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'pink',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input : {
+    width: 200, 
+    borderColor: 'gray', 
+    borderWidth: 1,
+    backgroundColor: 'white',
+  },
+  history : {
+    width:200,
+    alignItems: 'center',
+  }
 });
